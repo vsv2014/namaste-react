7fe811bdc918412afed7a3327d2e9601028e0821
vsv2014 <santhoshvishal.v@research.iiit.ac.in>
2025-07-07 17:27:45 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,24 @@
+// const heading = React.createElement(
+//     "h1" , 
+//     {id : "heading", xyz: "abc"} , 
+//     "Hello Mama! Namaste");
+// console.log(heading);
+
+
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "This is heading 1"),
+        React.createElement("h2", {}, "This is heading 2"), // Changed to h2 for variety
+    ]),
+]); 
+
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "This is heading 1"),
+        React.createElement("h2", {}, "This is heading 2"), // Changed to h2 for variety
+    ]);
+
+// Missing closing parenthesis and semicolon for the parent element
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent); // You should render 'parent', not 'heading'
